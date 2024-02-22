6d9c0dbab3c1dde41476801eb1d6351bed25cd6e
MaggieLOL <tanhanze@qq.com>
2024-02-22 20:06:40 +0800

    Add ovUI By cmd1152.

---
+++ plugins/ovui/index.js
@@ -0,0 +1,5 @@
+let link = document.createElement('link');  
+link.rel = 'stylesheet';  
+link.type = 'text/css';  
+link.href = 'https://plugins.hach.chat/plugins/ovui/index.css'; 
+document.getElementsByTagName('head')[0].appendChild(link);
