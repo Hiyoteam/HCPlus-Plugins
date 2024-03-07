d86c405db58a8f42200f822f3d791c16e3cc8fef
cmd1152 <cmd1152@qq.com>
2024-03-07 17:43:51 +0800

    add plugin

---
+++ plugins/fast_goto/index.js
@@ -0,0 +1,21 @@
+﻿run.goto = (...args) => {
+  if (args.length != 1) {
+    pushMessage({
+      nick: '!',
+      text: "".concat(args.length, " arguments are given while 1 is needed.")
+    });
+    return;
+  }
+  ws.onclose = () => {};
+  ws.close();
+  join(args[0]);
+  location.hash = myNick;
+  myChannel = args[0];
+}
+hook.register("before", "connect", (args) => {
+  setTimeout(()=>{
+    location.hash = '';
+    window.scrollTo(0, document.body.scrollHeight);
+  },500);
+  return args;
+});
