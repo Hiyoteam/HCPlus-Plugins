686fb09b8ccc255c6f3a8e72d0df6a8650881b9f
cmd1152 <cmd1152@qq.com>
2024-03-29 22:24:46 +0800

    fix

--- plugins/fast_know_kick_me/index.js
+++ plugins/fast_know_kick_me/index.js
@@ -1,12 +1,17 @@
 ﻿var checkString = false
+var joined = false
 hook.register('before','pushmessage', (args) =>{
+  if (args.cmd == "onlineSet") joined = true
   if (args.cmd == "warn" && args.text == 'Unknown command: /y' && checkString) {
     checkString = false
     return false;
   }
 })
 setInterval(()=>{
-  if (ws.readyState != 1 || checkString) return;
+  if (ws.readyState != 1 || checkString || !joined) {
+    joined = false;
+    return;
+  }
   checkString = true;
   ws.send(JSON.stringify({
     cmd: 'chat',
