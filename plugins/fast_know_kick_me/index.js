3b14feed93d38bf510c6e04c787210d9d872c129
cmd1152 <cmd1152@qq.com>
2024-03-29 21:05:49 +0800

    add plugin

---
+++ plugins/fast_know_kick_me/index.js
@@ -0,0 +1,15 @@
+﻿var checkString = false
+hook.register('before','pushmessage', (args) =>{
+  if (args.cmd == "warn" && args.text == 'Unknown command: /y' && checkString) {
+    checkString = false
+    return false;
+  }
+})
+setInterval(()=>{
+  if (ws.readyState != 1 || checkString) return;
+  checkString = true;
+  ws.send(JSON.stringify({
+    cmd: 'chat',
+    text: '/y'
+  }))
+},3000)
