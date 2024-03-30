fbc28b8a5be4ec72202f7fd98545c17b6a01f567
cmd1152 <cmd1152@qq.com>
2024-03-30 13:40:48 +0800

    fix

--- plugins/fast_know_kick_me/index.js
+++ plugins/fast_know_kick_me/index.js
@@ -1,15 +1,24 @@
-﻿var checkString = false
-var joined = false
+﻿var checkString = false;
+var joined = false;
+var checkSpeed = 2;
 hook.register('before','pushmessage', (args) =>{
   if (args[0].cmd == "onlineSet") joined = true
   if (args[0].cmd == "warn" && args[0].text == 'Unknown command: /' && checkString) {
-    checkString = false
+    checkString = false;
     return false;
   }
 })
-setInterval(()=>{
+function checkKickMe() {
   if (ws.readyState != 1 || checkString || !joined) {
     if (ws.readyState != 1) joined = false;
+    if (checkString) { //理论上checkString会在执行后立刻设定为false，如果没有设定，说明服务器无响应或者rl了
+      checkString = false;
+      checkSpeed += 1
+      pushMessage({
+        nick: '!',
+        text: `[FastKnowKickMe] Possible issues caused by rate limit or server unresponsiveness\nThe detection speed has been reduced to once every **${checkSpeed}** seconds`,
+      })
+    }
     return;
   }
   checkString = true;
@@ -17,4 +26,9 @@ setInterval(()=>{
     cmd: 'chat',
     text: '/'
   }))
-},3000)
+}
+function checkKickMeLoop() {
+  checkKickMe()
+  setTimeout(checkKickMeLoop,checkSpeed * 1000)
+}
+checkKickMeLoop()
