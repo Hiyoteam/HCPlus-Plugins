06f1bdd715bf04726d9a966cb9186cc3b29475d7
0x24a <tanhanze@qq.com>
2024-07-13 12:21:15 +0800

    Add send-panguify

--- plugins/pangu/pangu-module.js
+++ plugins/pangu/pangu-module.js
@@ -5,6 +5,13 @@ function panguPushHook(args){
     }
     return args
 }
+function panguSendHook(args){
+    if(args[0].cmd == "chat"){
+        args[0].text = pangu.spacing(args[0].text)
+    }
+    return args
+}
 hook.register("before", "pushmessage", panguPushHook)
+hook.register("in", "send", panguSendHook)
 pangu.spacingPageBody()
 console.log("Pangu Loaded!")
