b8b4242bd1767e9dba004eaf061fd1007a69f97c
0x24a <tanhanze@qq.com>
2024-07-13 11:46:21 +0800

    Improve

--- plugins/pangu/pangu-module.js
+++ plugins/pangu/pangu-module.js
@@ -1,8 +1,10 @@
 import pangu from 'https://cdn.skypack.dev/pangu@4.0.7';
 var original_pushMessage = pushMessage
 function pushMessage(args, options = {},padId="messages",makeunread=true){
+    if(args.text){
+        args.text=pangu.spacing(args.text)
+    }
     original_pushMessage(args, options, padId, makeunread)
-    pangu.autoSpacingPage()
 }
 pangu.autoSpacingPage()
 console.log("Pangu Loaded!")
